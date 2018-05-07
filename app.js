a4af3c2437ce76c79d586390a5ce573c95291ff7
James Stuckey <jstuckey@rutgers.edu>
2018-05-07 16:26:59 -0400

    modified whoAmI function in app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function whoAmI() {
+  var yearOfBirth = 2018 - age;
   console.log('Hi, my name is Alex and I\'m 31 years old');
+  console.log(`I was born in ${yearOfBirth}.`);
 }
 
 whoAmI();
