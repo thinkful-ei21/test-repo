49a668426823ea667456cd50c031b2fab2573e93
James Stuckey <jstuckey@rutgers.edu>
2018-05-07 17:38:32 -0400

    added beyond function

--- app2.js
+++ app2.js
@@ -1,4 +1,25 @@
+'use strict';
+
 function jediName (firstName, lastName) {
     return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 console.log(jediName('Alex', 'Widner'));
+
+function beyond (num) {
+    if (num === Infinity) {
+        return 'And beyond';
+    }
+    else if (num > 0) {
+        return 'To infinity';
+    }
+
+    else if (num < 0) {
+        return 'To negative infinity';
+    }
+
+    else {
+        return 'Staying home';
+    }
+}
+
+console.log(beyond(Infinity));
