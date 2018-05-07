108ebaaad2560f8b458d3e1612025a68a2299260
Alex Widner <manny1806@gmail.com>
2018-05-07 13:43:01 -0700

    added born function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
 
+function yearOfBirth (age){
+  return 2018 - age;
+}
 
-function whoAmI(name, age) {
-  var yearOfBirth = 2018 - age;  
-  console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
-  console.log(`I was born in ${yearOfBirth}.`);
+function whoAmI (name, age) {  
+  console.log (`Hi, my name is ${name} and I\'m ${age} years old`);
+  console.log (`I was born in ${yearOfBirth(age)}.`);
 }
 
 whoAmI('Alex', 31);
