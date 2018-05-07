4053fede6aa74e40a1b777a4d4f20fa8b3886eda
James Stuckey <jstuckey@rutgers.edu>
2018-05-07 16:57:06 -0400

    added try/catch to whoAmI function

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ function yearOfBirth (age){
   return 2018 - age;
 }
 
-function whoAmI (name, age) {  
+
+function whoAmI (name, age) {
+  try {
+    if (age < 0) {
+      throw new Error();
+    }
   console.log (`Hi, my name is ${name} and I\'m ${age} years old`);
   console.log (`I was born in ${yearOfBirth(age)}.`);
+  }
+
+  catch(e) {
+    console.error('Age cannot be a negative number.');
+  }
 }
 
-whoAmI('Alex', 31);
+whoAmI('Alex', -20);
